820154f707c5a7d487e2fbbcb6af4dfc831849c5
GabrielYukioMC <gabriel.coca@sptech.school>
2023-04-06 09:44:22 -0300

    maskeiko

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const mode = document.getElementById('tema');
+
+mode.addEventListener('click', () =>{
+    if (mode.classList.contains('fa-sun')) {
+        
+        alert('caixa do sol');
+    }
+})
