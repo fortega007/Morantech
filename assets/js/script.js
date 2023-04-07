484189a1496bdfe6df0f6f1b51a348bfcbbba995
GabrielYukioMC <gabriel.coca@sptech.school>
2023-04-07 18:26:00 -0300

    Dark Mode -v1.2.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,32 @@
-const mode = document.getElementById('tema');
 
-mode.addEventListener('click', () =>{
-    if (mode.classList.contains('fa-sun')) {
+var mode = document.getElementById('tema');
+var body = document.getElementById('body');
+var menu = document.getElementById('menu');
+var sectBI = document.getElementById('section_banner_inicial');
+var titulo = document.getElementById('titulo');
+var secC = document.getElementById('section_caixa')
+
+mode.addEventListener('click', () => {
+    if (mode.classList.contains('fa-moon')) {
+        mode.classList.remove('fa-moon');
+        mode.classList.add('fa-sun');
+
+        sectBI.classList.add('dark_mode');
+        menu.classList.add('dark_mode');
+        body.classList.add('dark_mode');
+        titulo.classList.add('dark_mode');
+        secC.classLista.add('dark_mode');
         
-        alert('caixa do sol');
+    }
+    else {
+        mode.classList.remove('fa-sun');
+        mode.classList.add('fa-moon');
+
+        sectBI.classList.remove('dark_mode');
+        menu.classList.remove('dark_mode');
+        body.classList.remove('dark_mode');
+        titulo.classList.remove('dark_mode');
+        secC.classLista.remove('dark_mode');
+        
     }
 })
