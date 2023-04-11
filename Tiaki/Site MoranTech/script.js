ea90ace4ceb6e66626b0fec8531cbf0ce0be7e11
TiakiWCardozo <125319667+TiakiWCardozo@users.noreply.github.com>
2023-04-10 23:49:31 -0300

    Serviços

---
+++ Tiaki/Site MoranTech/script.js
@@ -0,0 +1,7 @@
+window.sr = ScrollReveal({ reset: true });                ;
+
+sr.reveal('.caixa1', {duration: 3000});
+sr.reveal('.caixa2', {duration: 3000});
+sr.reveal('.caixa3', {duration: 3000});
+sr.reveal('.caixa4', {duration: 3000});
+
